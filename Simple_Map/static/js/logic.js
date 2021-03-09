23c01ba7e0952cfcb68c82c76f6b6b9cffdca23f
Josh Pearson <jipearsonua@gmail.com>
2021-03-09 14:51:51 -0600

    adding final logic

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -7,10 +7,10 @@ console.log("why is this not updating?")
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
+    id: 'dark-v10',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
