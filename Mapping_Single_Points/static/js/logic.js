6baa377f996b92cdd45fc1b8f1010a9c4821f5db
Josh Pearson <jipearsonua@gmail.com>
2021-03-08 18:59:40 -0600

    adding cities file

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -1,29 +1,24 @@
 // Add console.log to check to see if our code is working.
-console.log("working test");
-console.log("why is this not updating?")
+console.log("working");
 
 // Create the map object with a center and zoom level.
 // The 'mapid' will reference the id tag in our <div> element on the index.html file.
-let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+let map = L.map('mapid').setView([38.0522, -100.2437], 4);
 
-//  Add a marker to the map for Los Angeles, California.
-let marker = L.circle([34.0522, -118.2437], {
-    radius: 300,
-    color: 'black',
-    fillColor: '#ffffa1'
-}).addTo(map);
+// Get data from cities.js
+let cityData = cities;
 
-// Marker for home
-let home = L.marker([36.1060761565529, -86.73988331848427]).addTo(map);
-
-// Marker for lakeview
-let lakeview = L.circleMarker([44.0657021621797, -86.38146084434723]).addTo(map);
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.marker(city.location).addTo(map);
+   });  
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/dark-v10',
+    id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
