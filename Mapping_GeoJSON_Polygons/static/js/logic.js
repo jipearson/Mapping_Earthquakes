2adcd30fc4c8c7b37f42f59ff96b6cc93ff1354b
Josh Pearson <jipearsonua@gmail.com>
2021-03-12 16:49:31 -0600

    adding polygon code

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -37,33 +37,34 @@ let baseMaps = {
 // The 'mapid' will reference the id tag in our <div> element on the index.html file.
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  center: [44.0, -80.0],
+  center: [43.7, -79.3],
   zoom: 11,
-  layers: [satelliteStreets]
+  layers: [streets]
 });
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-// Accessing the Toronto hoods GeoJSON URL
-let torontoData  = "https://raw.githubusercontent.com/jipearson/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/Mapping_GeoJSON_Linestrings/torontoRoutes.json";
+// Accessing the Toronto Routes GeoJSON URL
+let torontoHoods  = "https://raw.githubusercontent.com/jipearson/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/Mapping_GeoJSON_Polygons/torontoNeighborhoods.json";
 
 
 // Create a style for the lines.#ffffa1
 let myStyle = {
-  color: "red",
-  weight: 2
+  color: "blue",
+  fillColor: "yellow",
+  weight: 1
 };
 
 // Grabbing our airport GeoJSON data.
-d3.json(torontoData).then(data =>{
+d3.json(torontoHoods).then(data =>{
   console.log(data);
-// Creating a GeoJSON layer with the retrieved data.
-L.geoJson(data, {
-  style: myStyle,
-  onEachFeature: function(feature, layer) {
-    // console.log(layer);
-    layer.bindPopup(`<h3>Airline: ${feature.properties.airline}</h3><hr><h3>Destination: ${feature.properties.dst}</h3>`)
-  }}).addTo(map);
-});
+    // Creating a GeoJSON layer with the retrieved data.
+    L.geoJson(data, {
+      style: myStyle,
+      onEachFeature: function(feature, layer) {
+        layer.bindPopup(`<h1>Neighborhood ${feature.properties.AREA_NAME}</h1> <hr> <h2>${feature.properties.AREA_S_CD}</h2>`);
+      }
+  }).addTo(map);
+  });
 
